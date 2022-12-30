7af975d284275c198adf477f272685bf30b01439
thinquepad <jonathan.giacomelli@ibm.com>
2022-12-30 01:42:29 -0600

    can spawn crystals anywhere, delete on click almost works

--- solar2d_app/thegame.lua
+++ solar2d_app/thegame.lua
@@ -11,18 +11,18 @@ local json = require ("json")
 -- defined local (scene) functions here 
 local titleBar, navBar, myMap, addressBar 
 local attempts = 0 
--- local markers = {}
+ markers = {}
 -- local markers = {{lat:30.6389954,long:-97.6856937},
 -- 	{lat:30.6389954,long:-97.6856937},
 -- 	{lat:30.6389954,long:-97.6856937},
 -- 	{lat:30.6389954,long:-97.6856937}}
-local markers = {{30.6389954,-97.6856937},{30.6389954,-97.6856937}}
-local imgFiles = {"blue_crystal1.png","orange_crystal2.png"}
+-- local markers = {{30.6389954,-97.6856937},{30.6389954,-97.6856937}}
+ imgFiles = {"blue_crystal1.png","orange_crystal2.png"}
 
-local homeURL = "https://auroras.fusionbombsderp.com"
-local saveFile = "AuroraOasisSave.id"
+ homeURL = "https://auroras.fusionbombsderp.com"
+ saveFile = "AuroraOasisSave.id"
 
-local token = composer.getVariable( "token" )
+ token = composer.getVariable( "token" )
 
 
 print ("JGDEBUG " .. " running ")
@@ -40,6 +40,13 @@ end
 local function isEmpty(s)
   return s == nil or s == ''
 end
+local function isTableEmpty( t )
+	local next = next
+	if next(t) == nil then
+	   return true
+	end
+	return false
+end
 
 local function convLatToM( lat )
 	return lat * 111139
@@ -48,15 +55,90 @@ local function convMtoLat( m )
 	return m / 111139
 end
 
+local function sendGETRequest( url , queriesConcat )
+	-- body
+	if isEmpty(queriesConcat)then
+		network.request( homeURL .. url, "GET", networkListenerGETRequest )
+	else -- ?count=30
+		network.request( homeURL .. url .. "?" .. queriesConcat, "GET", networkListenerGETRequest )
+	end
+
+end
+
+
+
+local function networkListenerSendDelete( event )
+	-- body
+	local id = nil
+	if ( event.isError ) then
+        print( "Network error: ", event.response )
+		native.showAlert( "Network Error: ", event.response, { "____" } ) 
+    else
+        print ( "RESPONSE: networkListenerSendDelete: " .. event.response )
+        -- local dataFromFile = networkListenerSendDelete(saveFile,event.response)
+        -- print ("change game scene now! ", dataFromFile)
+		-- composer.setVariable( "token", dataFromFile ) 
+		-- composer.gotoScene( "thegame" )
+
+		-- local decode = json.decode( event.response )
+		-- id = decode[i].id
+		-- native.showAlert( event.response, "decode: " .. decode, { "____" } ) 
+		-- native.showAlert( event.response, "id: "..id .. "," .. decode, { "____" } ) 
+		local id = event.response
+		for i=1,table.getn(markers) do
+			local line = markers[i]
+			if line 
+					and not isEmpty(line[1]) 
+					and not isEmpty(line[2]) 
+					and not isEmpty(line[3]) 
+					and not isEmpty(line[4]) 
+					and not isEmpty(line[5]) 
+					then
+				if line[4] == id then
+					-- we have a match for the deleted id, remove for markers
+					-- id = line[4]
+					table.remove(markers , i)
+					myMap:removeMarker( line[5] )
+				end
+			else
+			end
+		end 
+	end
+end
+local function sendDelete( id )
+	-- body
+	network.request( homeURL .. "/sendDelete?id="..id, "GET", networkListenerSendDelete )
+
+end
+
 local function markerListener(event)
     print( "type: ", event.type )  -- event type
     print( "markerId: ", event.markerId )  -- ID of the marker that was touched
     print( "lat: ", event.latitude )  -- latitude of the marker
     print( "long: ", event.longitude )  -- longitude of the marker
-	local outMsg = "you touched crystal at: " .. event.latitude .. ", " .. event.longitude
+	local outMsg = "you touched crystal at: " .. event.latitude .. ", " .. event.longitude .. "|" .. event.markerId
 
-	native.showAlert( "MARKER1", outMsg, { "<3" } ) 
+	-- native.showAlert( "MARKER1", outMsg, { "<3" } ) 
+	local id = nil
+	for i=1,table.getn(markers) do
+		local line = markers[i]
+		if line 
+				and not isEmpty(line[1]) 
+				and not isEmpty(line[2]) 
+				and not isEmpty(line[3]) 
+				and not isEmpty(line[4]) 
+				and not isEmpty(line[5]) 
+				then
+			if line[5] == event.markerId then
+				-- we have a match for markerId, so grab _id and do db.delete()
+				-- id = line[4]
+				sendDelete( line[4] )
+			end
+		else
+			-- native.showAlert( "Error: ", "Cant read markers[][] ", { " :'( " } ) 
 
+		end
+	end 
 end
 
 -- local function getCurrentLocation( )
@@ -82,6 +164,40 @@ local function getCurrentLocation( )
 	end 
 end
 
+local function placeAllMarkers( currentLocation )
+	-- body
+	myMap:setCenter( currentLocation.latitude, currentLocation.longitude ) 
+	myMap:setRegion( currentLocation.latitude, currentLocation.longitude, 0.01, 0.01) 
+	local options = { 
+		title="You are here", 
+		subtitle="testing this", 
+		listener = markerListener, 
+		imageFile =  "img/blue_crystal1.png",
+	} 
+	if markers and not isTableEmpty(markers) and not isEmpty(markers[1][1]) then 
+		for i=1,table.getn(markers) do
+			local line = markers[i]
+			if line 
+					and not isEmpty(line[1]) 
+					and not isEmpty(line[2]) 
+					and not isEmpty(line[3]) 
+					and not isEmpty(line[4]) 
+					then
+				options["subtitle"] = "This is crystal#: " .. i
+				options["imageFile"] = "img/" .. imgFiles[line[3]]
+				-- options["_id"] = line[4]
+				local result = myMap:addMarker( line[1],line[2], options )
+				table.insert(markers[i], result)
+				print("markers " .. i .. "line: " .. line[1] .. "," .. line[2] .. "," .. line[3])
+			else
+				-- native.showAlert( "Error: ", "Cant read markers[][] ", { " :'( " } ) 
+
+			end
+		end 
+	end
+	-- native.showAlert( "mLH", "markers " .. markers[1][1] .. "," .. markers[2][1], { "###" } ) 
+end
+
 local function maplocationHandler( event ) 
 	local currentLocation = myMap:getUserLocation() 
 	-- print ("JGDEBUG " .. currentLocation)
@@ -93,27 +209,27 @@ local function maplocationHandler( event )
 			timer.performWithDelay( 1000, maplocationHandler ) 
 		end 
 	else 
-		myMap:setCenter( currentLocation.latitude, currentLocation.longitude ) 
-		myMap:setRegion( currentLocation.latitude, currentLocation.longitude, 0.01, 0.01) 
-		local options = { 
-			title="You are here", 
-			subtitle="testing this", 
-			listener = markerListener, 
-			imageFile =  "img/blue_crystal1.png",
-		} 
-		if not isEmpty(markers[1][1]) then 
-			for i=1,table.getn(markers) do
-				local line = markers[i]
-				if line and not isEmpty(line[1]) and not isEmpty(line[2]) and not isEmpty(line[3]) then
-					options["subtitle"] = "This is crystal#: " .. i
-					options["imageFile"] = "img/" .. imgFiles[line[3]]
-					myMap:addMarker( line[1],line[2], options )
-					print("markers " .. i .. "line: " .. line[1] .. "," .. line[2] .. "," .. line[3])
-				end
-			end 
-		end
-		native.showAlert( "mLH", "markers " .. markers[1][1] .. "," .. markers[2][1], { "###" } ) 
-
+		-- myMap:setCenter( currentLocation.latitude, currentLocation.longitude ) 
+		-- myMap:setRegion( currentLocation.latitude, currentLocation.longitude, 0.01, 0.01) 
+		-- local options = { 
+		-- 	title="You are here", 
+		-- 	subtitle="testing this", 
+		-- 	listener = markerListener, 
+		-- 	imageFile =  "img/blue_crystal1.png",
+		-- } 
+		-- if not isEmpty(markers[1][1]) then 
+		-- 	for i=1,table.getn(markers) do
+		-- 		local line = markers[i]
+		-- 		if line and not isEmpty(line[1]) and not isEmpty(line[2]) and not isEmpty(line[3]) then
+		-- 			options["subtitle"] = "This is crystal#: " .. i
+		-- 			options["imageFile"] = "img/" .. imgFiles[line[3]]
+		-- 			myMap:addMarker( line[1],line[2], options )
+		-- 			print("markers " .. i .. "line: " .. line[1] .. "," .. line[2] .. "," .. line[3])
+		-- 		end
+		-- 	end 
+		-- end
+		-- native.showAlert( "mLH", "markers " .. markers[1][1] .. "," .. markers[2][1], { "###" } ) 
+		placeAllMarkers(currentLocation)
 		-- myMap:addMarker( currentLocation.latitude, currentLocation.longitude, options )
 
 		-- myMap:addMarker( currentLocation.latitude, currentLocation.longitude + convMtoLat(1000) , options ) 
@@ -144,9 +260,9 @@ local function networkListenerGetCrystals( event )
 		for i=1,table.getn(decode) do
 			-- print(i)
 			local line = decode[i]
-			table.insert(markers, {line.lat, line.long, line.type})
+			table.insert(markers, {line.lat, line.long, line.type, line._id})
 		end
-
+		placeAllMarkers(getCurrentLocation())
 		-- print("decide: #: " #decode .. "," .. decode)
     end
 end
@@ -267,36 +383,40 @@ function scene:show( event )
 
 
 		-- getCrystals()
+		local testTable={}
+		if isTableEmpty(testTable) then
+			print ("works!!") 
+		end
+		print ("isTableEmpty: ",isTableEmpty(testTable),testTable) 
 
 		-- print(token)
 		-- print(string.sub(token,1,7))
 		local userIDText = display.newText( "Your ID: " .. string.sub(token,1,7), 80, 0, native.systemFont, 16 )
 		userIDText:setFillColor( 1, 0, 1 )
 
-		-- local testX1 = display.newText( "-20", -20, 200, native.systemFont, 16 )
-		-- local testX2 = display.newText( "0", 0, 200, native.systemFont, 16 )
-		-- local testX3 = display.newText( "20", 20, 200, native.systemFont, 16 )
-		-- local testX4 = display.newText( "100", 100, 200, native.systemFont, 16 )
-		-- local testX5 = display.newText( "200", 200, 200, native.systemFont, 16 )
-		-- local testX6 = display.newText( "300", 300, 200, native.systemFont, 16 )
-		-- local testY1 = display.newText( "-20", 50, -20, native.systemFont, 16 )
-		-- local testY2 = display.newText( "0", 50, 0, native.systemFont, 16 )
-		-- local testY3 = display.newText( "20", 50, 20, native.systemFont, 16 )
-		-- local testY4 = display.newText( "50", 50, 50, native.systemFont, 16 )
-		-- local testY5 = display.newText( "100", 50, 100, native.systemFont, 16 )
-		-- local testY6 = display.newText( "200", 50, 200, native.systemFont, 16 )
-		-- local testY7 = display.newText( "300", 50, 300, native.systemFont, 16 )
-		-- local testY7 = display.newText( "400", 50, 400, native.systemFont, 16 )
-		-- local testY7 = display.newText( "500", 50, 500, native.systemFont, 16 )
-		-- userIDText:setFillColor( 0, 0, 1 )
+		local testX1 = display.newText( "-20", -20, 200, native.systemFont, 16 )
+		local testX2 = display.newText( "0", 0, 200, native.systemFont, 16 )
+		local testX3 = display.newText( "20", 20, 200, native.systemFont, 16 )
+		local testX4 = display.newText( "100", 100, 200, native.systemFont, 16 )
+		local testX5 = display.newText( "200", 200, 200, native.systemFont, 16 )
+		local testX6 = display.newText( "300", 300, 200, native.systemFont, 16 )
+		local testY1 = display.newText( "-20", 50, -20, native.systemFont, 16 )
+		local testY2 = display.newText( "0", 50, 0, native.systemFont, 16 )
+		local testY3 = display.newText( "20", 50, 20, native.systemFont, 16 )
+		local testY4 = display.newText( "50", 50, 50, native.systemFont, 16 )
+		local testY5 = display.newText( "100", 50, 100, native.systemFont, 16 )
+		local testY6 = display.newText( "200", 50, 200, native.systemFont, 16 )
+		local testY7 = display.newText( "300", 50, 300, native.systemFont, 16 )
+		local testY7 = display.newText( "400", 50, 400, native.systemFont, 16 )
+		local testY7 = display.newText( "500", 50, 500, native.systemFont, 16 )
 
 		-- part of the map will go below the standard viewport
-		myMap = native.newMapView(0, 0,display.contentWidth , display.contentHeight -60) 
+		myMap = native.newMapView(0, 0,display.contentWidth , display.contentHeight -40) 
 		if myMap then 
 			myMap.mapType = "standard" 
 			myMap.x = display.contentCenterX 
 			-- navBar height is 52 per api doc // deleted!
-			myMap.y = display.contentCenterY - 140
+			myMap.y = display.contentCenterY - 40
 			local currentLocation = maplocationHandler() 
 			-- local myText = display.newText( "Hello World!", 100, 20, native.systemFont, 16 )
 			-- myText:setFillColor( 1, 0, 0 )
--- www_server/www/index.js
+++ www_server/www/index.js
@@ -6,6 +6,7 @@ const config = require('./config');
 const session = require('express-session');
 const crypto = require('crypto');
 
+var ObjectId = require('mongodb').ObjectID;
 var bodyParser = require('body-parser')
 var validate = require('jsonschema').validate;
 // console.log(validate(4, {"type": "number"}));
@@ -34,6 +35,7 @@ app.use(session({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'static')));
+const oneHour = 60 * 60 * 1001 /* ms */
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -43,12 +45,33 @@ function getRndGPSinM(center, distance) {
   return center + convMtoGPS(meters)
 }
 function convMtoGPS(m){
-  return m / 111111 
+  return parseFloat(m / 111111)
 }
 function convGPStoM(gps){
-  return gps * 111111
+  return parseFloat(gps * 111111)
+}
+function getRndGPSPairFromCenter(lat, long, radius){
+  var gps = {}
+  gps.lat = getRndGPSinM(lat, radius) // my lat, long
+  gps.long = getRndGPSinM(long, radius)
+  return gps
+}
+function getRndGPSNearMe(radius=1000){
+  return getRndGPSPairFromCenter(30.6389954, -97.6856937, radius)
+}
+  //31.841401, -98.884759 // 321,800 // center-is of texas to closest borders (mexico and gainesville)
+function getRndGPSinTexas(lat=31.841401, long=-98.884759,radius=353800){
+  return getRndGPSPairFromCenter(lat, long, radius)
 }
 
+function getRndGPSRndMethod(){
+  var method = getRndInteger(1,10)
+  if(method = 1){
+
+  }else{
+
+  }
+}
 
 MongoClient.connect(config.db, (err, client) => {
   if (err) return console.error(err)
@@ -58,6 +81,47 @@ MongoClient.connect(config.db, (err, client) => {
   const testColl = db.collection('testColl')
   const userColl = db.collection('userColl')
   const crystalColl = db.collection('crystalColl')
+
+  function refreshCrystals(res){
+    var dtNow = Date.now()
+    var tooOld = dtNow 
+    // var tooOld = dtNow - 12 * oneHour
+    crystalColl.deleteMany( { timeCreated: { $lt: tooOld } } )
+    .then(result => {
+      console.log("refreshCrystals result:", result, dtNow, tooOld)
+      // res.send(JSON.stringify(result) + 'Deleted some crystals!')
+      if (result.deletedCount) {inventCountCrystals(res, result.deletedCount, false, result)}
+    })
+    .catch(error => console.error(error))
+
+  }
+
+  function inventCountCrystals(res, count=10, me=false, didDelete=""){
+    bulkUpdateOps = [];  
+    for (var i = 0; i < count; i++) {
+      var crystalElem = {}
+      if(me){
+        crystalElem = getRndGPSNearMe()
+      }else{
+        crystalElem = getRndGPSinTexas()
+      }
+      
+      // crystalElem.lat = getRndGPSinM(30.6389954, 1000)
+      // crystalElem.long = getRndGPSinM(-97.6856937, 1000)
+      crystalElem.type = getRndInteger(1,2)
+      crystalElem.timeCreated = Date.now()
+
+      bulkUpdateOps.push({ "insertOne": { "document": crystalElem } });
+    }
+
+    if (bulkUpdateOps.length > 0) {
+      crystalColl.bulkWrite(bulkUpdateOps).then(function(r) {
+          // do something with result
+        console.log("inventCountCrystals(): ",r);
+        res.send('Invented count crystals, '+count+"|"+didDelete);
+      });
+    }
+  }
 
   app.get('/', (req, res) => {
     // const cursor = db.collection('quotes').find()
@@ -161,15 +225,24 @@ MongoClient.connect(config.db, (err, client) => {
 
   app.post('/getCrystals', function(req, res) {
     // Capture the input fields
-    let mylat = req.body.mylat;
-    let mylong = req.body.mylong;
+    var range = 1000 // meters
+    var gpsRange = parseFloat(convMtoGPS(range))
+    let mylat = parseFloat(req.body.mylat);
+    let mylong = parseFloat(req.body.mylong);
     // Ensure the input fields exists and are not empty
     if (mylat && mylong) {
-
+      // console.log("query: ", mylat, mylong, gpsRange)
+      // var testVar = mylat+gpsRange
+      // console.log("query2: ", testVar)
+      // { price: { $ne: 1.99, $exists: true } }
       crystalColl.find({ $and: [  
-            { $or: [ { lat: {$gt: mylat - 1} }, { lat: {$lt: mylat + 1} } ] }, 
-            { $or: [ { long: {$gt: mylong -1} }, { long: {$lt: mylong + 1} } ] } 
+            { lat: {$gt: mylat - gpsRange , $lt: mylat + gpsRange} }, 
+            { long: {$gt: mylong - gpsRange , $lt: mylong + gpsRange} } 
           ]} ).toArray()
+      // crystalColl.find({ $and: [  
+      //       { lat: {$gt: 29 , $lt: 31} }, 
+      //       { long: {$gt: -98 , $lt: -96} } 
+      //     ]} ).toArray()
           // ]} ).limit(2).toArray()
       .then(results => {
         // console.log("after get", results)
@@ -192,6 +265,27 @@ MongoClient.connect(config.db, (err, client) => {
       res.end();
     }
   });
+
+  app.get('/forceCrystalRefresh', (req, res) => {
+    refreshCrystals(res)
+  });
+
+
+  app.get('/inventCountCrystals', (req, res) => {
+    // let email = req.body.email;
+    var count = 10
+    var me = false
+    console.log("query: ",req.query)
+    if(req.query){
+      if(req.query.count){
+        count = req.query.count
+      }
+      if(req.query.me){
+        me = req.query.me
+      }
+    }
+    inventCountCrystals(res, count, me)
+  })
 
   app.get('/inventSomeCrystals', (req, res) => {
     // res.send('phone app made a get request!')
@@ -250,6 +344,40 @@ MongoClient.connect(config.db, (err, client) => {
     //   })
     //   .catch(error => console.error(error))
   })
+
+  app.get('/sendDelete', (req, res) => {
+    // let email = req.body.email;
+    // var count = 10
+    console.log("sendDelete query: ",req.query)
+    if(req.query && req.query.id){
+      // count = req.query.count
+      crystalColl.deleteOne( { "_id" : ObjectId(req.query.id) } )
+      // crystalColl.deleteOne( { "_id" : req.query.id} )
+      .then(result => {
+        console.log("sendDelete result:", result)
+        // res.send(JSON.stringify(result) + 'Deleted some crystals!')
+        if (result.deletedCount) {res.send(req.query.id)}
+      })
+      .catch(error => console.error(error))
+    }
+    // inventCountCrystals(res, count)
+    // send.res
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
